e89c15fce52031746357cdcf587c19c27812a86f
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-08-14 12:33:06 +0000

    added simplejson import to main

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import urllib
 import logging
 import pickle
 
+
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from google.appengine.api import memcache
+
+from django.utils import simplejson
 
 import models
 
