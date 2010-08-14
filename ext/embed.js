13be96cc0e62b562505d634e4280dd6a7016f649
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-08-14 21:00:03 +0000

    added sonatosh commit on css for chat text

--- ext/embed.js
+++ ext/embed.js
@@ -3,4 +3,4 @@ party_wrapper.class = "party_wrapper";
 var party_script = document.createElement("script");
 party_script.src = "http://party-page.appspot.com/party-page-js";
 party_wrapper.appendChild(party_script);
-document.body.appendChild(party_wrapper);
+document.getElementsByTagName("body")[0].appendChild(party_wrapper);
