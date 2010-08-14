033fa52145b546ccb329e935f0b09fd7d122fca3
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-08-14 19:56:50 +0000

    Extension and Image data

---
+++ ext/embed.js
@@ -0,0 +1,6 @@
+var party_wrapper = document.createElement("div");
+party_wrapper.class = "party_wrapper";
+var party_script = document.createElement("script");
+party_script.src = "http://party-page.appspot.com/party-page-js";
+party_wrapper.appendChild(party_script);
+document.body.appendChild(party_wrapper);
