a3f9aa9822417241182352e99f2be08c50ffbb3b
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-08-15 02:39:57 +0000

    god knows

--- ext/embed.js
+++ ext/embed.js
@@ -1,6 +1,8 @@
+var host = "http://localhost:8080/";
+// var host = "http://party-page.appspot.com/";
 var party_wrapper = document.createElement("div");
 party_wrapper.class = "party_wrapper";
 var party_script = document.createElement("script");
-party_script.src = "http://party-page.appspot.com/party-page-js";
+party_script.src = host + "party-page-js";
 party_wrapper.appendChild(party_script);
 document.getElementsByTagName("body")[0].appendChild(party_wrapper);
