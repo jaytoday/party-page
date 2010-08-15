8e579938cdd622a3efed858ba040905f5b8563ba
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-08-15 22:48:17 +0000

    new extension file

--- ext/embed.js
+++ ext/embed.js
@@ -1,8 +1,15 @@
 // var host = "http://localhost:8080/";
-var host = "http://party-page.appspot.com/";
-var party_wrapper = document.createElement("div");
-party_wrapper.class = "party_wrapper";
-var party_script = document.createElement("script");
-party_script.src = host + "party-page-js";
-party_wrapper.appendChild(party_script);
-document.getElementsByTagName("body")[0].appendChild(party_wrapper);
+var pp = document.getElementById("chat_wrapper");
+if(pp){
+	document.body.removeChild(pp);
+	var ct = document.getElementById("chat_tab");
+	document.body.removeChild(ct);
+}else{
+	var host = "http://party-page.appspot.com/";
+	var party_wrapper = document.createElement("div");
+	party_wrapper.class = "party_wrapper";
+	var party_script = document.createElement("script");
+	party_script.src = host + "party-page-js";
+	party_wrapper.appendChild(party_script);
+	document.getElementsByTagName("body")[0].appendChild(party_wrapper);
+}
